fd416ff99107d6686eab7d1fb9e59baa9f7cccc8
Paulo Melo <paulo@goclip.com.br>
2015-06-02 09:22:19 -0300

    [improv] extracting page number from HOCR Page

---
+++ src/test/java/com/googlecode/jhocr/HocrParserTest.java
@@ -0,0 +1,30 @@
+package com.googlecode.jhocr;
+
+import com.googlecode.jhocr.element.HocrDocument;
+import com.googlecode.jhocr.parser.HocrParser;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by paulo on 02/06/15.
+ */
+public class HocrParserTest {
+
+    @Test
+    public void shouldExtractPageNumber() {
+        InputStream hocr = this.getClass().getClassLoader()
+                .getResourceAsStream("test-data/jhocr_test_case_0001.html");
+        HocrDocument parse = new HocrParser(hocr).parse();
+
+        parse.getPages()
+                .stream()
+                .filter(page -> page.getPageNumber() != 0)
+                .findFirst()
+                .ifPresent(page -> assertEquals((int) page.getPageNumber(), 1));
+    }
+
+}
